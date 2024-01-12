e41adb410ea3812ea23f25f540d32c11933d488d
yusufbozkurt <yusufbozkurt2022@gmail.com>
2024-01-12 15:32:16 +0300

    some changes for security

---
+++ Data/Entities/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarketPlace.Data.Entities
+{
+	public class Cart : BaseItemEntity
+	{
+		[ForeignKey("UserId")]
+		public int UserId { get; set; }
+
+		[ForeignKey("ProductId")]
+		public int ProductId { get; set; }
+
+		public int Count { get; set; }
+	}
+}
